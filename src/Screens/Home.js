380c7710100fb34116db9387496974de4975bddb
Jonathan <joohny_2014@hotmail.com>
2022-10-03 09:43:52 -0300

    Home.css

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,7 +1,26 @@
+import "./Home.css"
 export default function Home() {
     return(
         <div>
-            <p>Home</p>
+            <div className="name-container">
+                <div className="in-container">
+                    <h1>Hi, <br/>
+                        I´m Jonathan, <br/> 
+                        a fullstack Web developer.
+                    </h1>
+                    <div className="down-container">
+                        <p>
+                            Front-end
+                            </p>
+                            <p>
+                            Back-end
+                            </p>
+                            <p>
+                            Fullstack
+                            </p>
+                    </div>
+                </div>
+            </div>            
         </div>
     )
 }
