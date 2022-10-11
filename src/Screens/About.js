df8f4f13ebd4378aff6a1fa75700eb67a53781a0
Jonathan <joohny_2014@hotmail.com>
2022-10-11 16:03:08 -0300

    About edited

--- src/Screens/About.js
+++ src/Screens/About.js
@@ -1,10 +1,18 @@
 import "./About.css"
 export default function About() {
+
+
     return(
-        <div className="main-about">
-            <div className="about">
-        
-            </div>
+        <div className="about-main">
+            <div className="fade"></div>
+            <section className="star-wars">
+                <div className="crawl">
+                    <div className="title">
+                        <h1>OLá mundo</h1>
+                    </div>
+                    <p>asdhasudias ipsuin dolor asdhasudias ipsuin dolor asdhasudias ipsuin dolor asdhasudias ipsuin dolor asdhasudias ipsuin dolor asdhasudias ipsuin dolor</p>
+                </div>
+            </section>
         </div>
     )
 }
