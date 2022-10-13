e290dd3daa0925cafb2d3b1ec41235b03700ec30
Jonathan <joohny_2014@hotmail.com>
2022-10-13 16:43:05 -0300

    App edited

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Navbar/>
       <Routes>
-        <Route path="/Home" element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
         <Route path="/projects" element={<Projects/>}/>
