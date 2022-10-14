4a667a00b18379f0f486ea2294642c34abda088c
Jonathan <joohny_2014@hotmail.com>
2022-10-13 22:10:03 -0300

    About star wars theme edited

--- src/Screens/About.js
+++ src/Screens/About.js
@@ -1,18 +1,59 @@
+import { useEffect, useRef, useState } from "react"
 import "./About.css"
 export default function About() {
 
+    const chapters=[
+        {id:0, value: "Hello world"},
+        {id:1, value: "Hello world"},
 
+    ]
+    const numbers=[
+        {id:0, value: "Chapter 1"},
+        {id:1, value: "Chapter 2"},
+
+    ]
+
+    const crawl = useRef(null)
+    const [chapterData, setChapterData]=useState(chapters[0].value)
+    const [numberData, setNumberData]=useState(numbers[0].value)
+
+    const handleChapter=(index)=>{
+        const chapterSelected=chapters[index].value
+        setChapterData(chapterSelected)
+        const numberSelected=numbers[index].value
+        setNumberData(numberSelected)
+        crawl.current.className = "crawl";
+        setTimeout(() => {
+            crawl.current.className = "crawl animation-crawl";
+        }, 1);
+    }
     return(
         <div className="about-main">
+            <div className="chapter-main">
+                <div className="chapters">
+                    <div className="chapter">
+                        <p onClick={()=>handleChapter(0)}>Chapter One</p>
+                    </div>
+                    <div className="chapter">
+                        <p onClick={()=>handleChapter(1)}>Chapter Two</p>
+                    </div>
+                    <div className="chapter">
+                        <p onClick={()=>handleChapter(2)}>Chapter Thre</p>
+                    </div>
+                    <div className="chapter">
+                        <p onClick={()=>handleChapter(3)}>Chapter Four</p>
+                    </div>
+                </div>
+            </div>
             <div className="fade"></div>
             <section className="star-wars">
-                <div className="crawl">
+                <div ref={crawl} className={"crawl animation-crawl"}>
                     <div className="title">
-                        <h1>Chapter 1</h1>
+                        <h1>{numberData}</h1>
+                        <br/>
                     </div>
-                    <p>A long long time ago...<br/>
-                        In a little blue planet,<br/>  there was a species, called, human beings<br/><br/>
-                        Our story talks about a little dreamer boy, called Jonathan
+                    <p>
+                        {chapterData}
                     </p>
                 </div>
             </section>
