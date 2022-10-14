8d4a32073fe259fc1326f9b42b2960a404c67b92
Jonathan <joohny_2014@hotmail.com>
2022-10-13 21:13:44 -0300

    Rework of style

--- src/Components/Projects/ProjectsContainer.js
+++ src/Components/Projects/ProjectsContainer.js
@@ -3,29 +3,33 @@ export default function ProjectsContainer() {
 
     return(
         <div className="projectsContainer">
-            <div className="backgroundContainer"></div>
+            <div className="backgroundContainer">
+            </div>
             <div className="projects">
                 <div className="project">
                     <p>Mario Kaboom Js</p>
-                    <img src="mario.jpg" alt="calculadora" />
-                </div>
-                <div className="project">
-                    <p>Calculator Js</p>
-                    <img id="calculadora" src="calculadora.jpg" alt="calculadora" />
+                    <div className="project-img">
+                        <img src="mario.jpg" alt="calculadora" />
+                    </div>
                 </div>
                 <div className="project">
                     <p>Fly swatter Js</p> 
-                    <img src="matamosca.jpg" alt="calculadora" />
+                    <div className="project-img">
+                        <img src="matamosca.jpg" alt="calculadora" />
+                    </div>
                 </div>
                 <div className="project">
                     <p>Thoughs Node</p> 
-                    <img src="thoughs.jpg" alt="thoughs" />
+                    <div className="project-img">
+                        <img src="thoughs.jpg" alt="thoughs" />
+                    </div>
                 </div>
                 <div className="project">
-                    <p>Thoth Laravel React</p> 
-                    <img src="thoth.jpg" alt="thoth" />
+                    <p>Thoth Laravel React</p>
+                    <div className="project-img">
+                        <img src="thoth.jpg" alt="thoth" />
+                    </div> 
                 </div>
-                
             </div>
         </div>
     )
